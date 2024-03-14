c6b4129239a38b4e93351c4663bf12ce8de228a4
trapvpack <vitost666@gmail.com>
2024-03-14 20:42:58 +0300

    added migrations for cards and columns

---
+++ database/migrations/2024_03_05_151155_create_cards_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cards', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name',255);
+            $table->foreignId('user_id');
+            $table->foreignId('column_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cards');
+    }
+};
